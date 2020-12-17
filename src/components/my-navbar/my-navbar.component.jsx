bf449f4408d8d27515a1ac4a73ad97867ab64074
tranpham <17520873@gm.uit.edu.vn>
2020-12-17 12:48:26 +0700

    push all

---
+++ src/components/my-navbar/my-navbar.component.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Logo from "../../assets/icons/logo.png";
+import "./my-navbar.styles.css";
+import  { Component } from "react";
+import classnames from "classnames";
+
+export default class MyNavbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      prevScrollpos: window.pageYOffset,
+      visible: true
+    };
+  }
+
+  // Adds an event listener when the component is mount.
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  // Remove the event listener when the component is unmount.
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  // Hide or show the menu.
+  handleScroll = () => {
+    const { prevScrollpos } = this.state;
+
+    const currentScrollPos = window.pageYOffset;
+    const visible = prevScrollpos > currentScrollPos;
+
+    this.setState({
+      prevScrollpos: currentScrollPos,
+      visible
+    });
+  };
+
+  render() {
+    return (
+      <Navbar
+      fixed="top"
+      variant="dark"
+      expand="md"
+        className={classnames("animate-navbar nav-theme justify-content-between", {
+          "navbar--hidden": !this.state.visible
+        })}
+      >
+        <div>
+          <Navbar.Brand href="#home">
+            <img className="logo" src={Logo} alt="" />
+          </Navbar.Brand>
+        </div>
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto ">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#skills">Skills</Nav.Link>
+              {/* <Nav.Link href="#experience">Experience</Nav.Link> */}
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Navbar>
+    );
+  }
+}
+
+
+// const MyNavbar = () => {
+//   return (
+//     <div>
+//       <Navbar
+
+//         className="animate-navbar nav-theme justify-content-between"
+//       >
+
+//       </Navbar>
+//     </div>
+//   );
+// };
+
+// export default MyNavbar;
---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "./about.styles.css";
+import Profile from "../../assets/img/profile/myAvatar.jpg";
+import Image from "react-bootstrap/Image";
+import Button from "react-bootstrap/Button";
+
+const About = () => {
+  return (
+    <div id="about">
+      <div className="about">
+        <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+        <Container>
+          <Row className="pt-3 pb-5 align-items-center">
+            <Col xs={12} md={6}>
+              <Row className="justify-content-center mb-2 mr-2 ">
+                <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+              </Row>
+            </Col>
+            <Col xs={12} md={6}>
+              <Row className=" align-items-start p-2 my-details rounded">
+                Hi there! My name is<strong>&nbsp;Trần Nguyễn Thành Phẫm</strong>
+                <br />I am studying at University of Information and Technology.
+                <br />My short-term goal is to find a suitable position for myself.
+                <br />My long-term goal is to work in a company enable me to improve my knowledge.                <br />
+                
+                <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="#contact">
+                      <Button className="m-2" variant="outline-primary">
+                        Let's talk
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://drive.google.com/file/d/1Snbq3x4Itk_ZN4c8J96aNdp4SunXR08Y/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-success">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/tranpham9999" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                  {/* <div>
+                    <a href="https://www.linkedin.com/in/anand-kumar-jha-745798a4/" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-info">
+                        LinkedIn
+                      </Button>
+                    </a>
+                  </div> */}
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
